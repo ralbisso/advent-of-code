dd804a8fa65588ab079767f54798cddc5e81847c
romain albisson <albisson.romain@gmail.com>
2023-03-30 13:07:52 +0200

    FileUtils change

--- advent-of-code/src/main/java/aoc2018/Day01.java
+++ advent-of-code/src/main/java/aoc2018/Day01.java
@@ -1,7 +1,6 @@
 package aoc2018;
 
 import java.util.HashSet;
-import java.util.List;
 
 import utils.FileUtils;
 import utils.enums.Day;
@@ -9,12 +8,12 @@ import utils.enums.Day;
 public class Day01 extends AoC2018 {
 
     private final Day day = Day.DAY_01;
-    private final List<String> input = FileUtils.getAllLines(year, day);
+    private final int[] input = FileUtils.getAllLinesAsIntArray(year, day);
 
     public int solvePart1() {
         int frequency = 0;
-        for (String change : input) {
-            frequency += Integer.parseInt(change);
+        for (int change : input) {
+            frequency += change;
         }
         return frequency;
     }
@@ -23,8 +22,8 @@ public class Day01 extends AoC2018 {
         int frequency = 0, index = 0;
         var history = new HashSet<>();
         while (history.add(frequency)) {
-            frequency += Integer.parseInt(input.get(index));
-            index = (index + 1) % input.size();
+            frequency += input[index];
+            index = (index + 1) % input.length;
         }
         return frequency;
     }
