50fe9f25f8865f003f9675d6e77832807e4968d9
romain albisson <albisson.romain@gmail.com>
2023-03-30 13:41:19 +0200

    2020 Day 1: Report Repair

--- advent-of-code/src/main/java/aoc2020/Day01.java
+++ advent-of-code/src/main/java/aoc2020/Day01.java
@@ -1,12 +1,38 @@
 package aoc2020;
 
-public class Day01 {
+import java.util.Arrays;
+
+import utils.FileUtils;
+import utils.enums.Day;
+
+public class Day01 extends AoC2020 {
+
+    private final Day day = Day.DAY_01;
+    private final int[] input = FileUtils.getAllLinesAsIntArray(year, day);
 
     public int solvePart1() {
+        Arrays.sort(input);
+        for (int entry : input) {
+            int difference = 2020 - entry;
+            if (Arrays.binarySearch(input, difference) > 0) {
+                return entry * difference;
+            }
+        }
         return 0;
     }
 
     public int solvePart2() {
+        Arrays.sort(input);
+        int length = input.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int firstTwoEntries = input[i] + input[j];
+                int difference = 2020 - firstTwoEntries;
+                if (Arrays.binarySearch(input, difference) > 0) {
+                    return input[i] * input[j] * difference;
+                }
+            }
+        }
         return 0;
     }
 }
