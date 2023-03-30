b03188be50c904e229bb078b1cc2c43a0949a320
romain albisson <albisson.romain@gmail.com>
2023-03-30 12:49:31 +0200

    2018 Day 1: Chronal Calibration

--- advent-of-code/src/main/java/aoc2018/Day01.java
+++ advent-of-code/src/main/java/aoc2018/Day01.java
@@ -1,12 +1,31 @@
 package aoc2018;
 
-public class Day01 {
+import java.util.HashSet;
+import java.util.List;
+
+import utils.FileUtils;
+import utils.enums.Day;
+
+public class Day01 extends AoC2018 {
+
+    private final Day day = Day.DAY_01;
+    private final List<String> input = FileUtils.getAllLines(year, day);
 
     public int solvePart1() {
-        return 0;
+        int frequency = 0;
+        for (String change : input) {
+            frequency += Integer.parseInt(change);
+        }
+        return frequency;
     }
 
     public int solvePart2() {
-        return 0;
+        int frequency = 0, index = 0;
+        var history = new HashSet<>();
+        while (history.add(frequency)) {
+            frequency += Integer.parseInt(input.get(index));
+            index = (index + 1) % input.size();
+        }
+        return frequency;
     }
 }
