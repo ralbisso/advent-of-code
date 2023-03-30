15d3073eaf3af348aed7e9e93f627f27ef6d9e69
romain albisson <albisson.romain@gmail.com>
2023-03-30 12:28:20 +0200

    2017 Day 1: Inverse Captcha

--- advent-of-code/src/main/java/aoc2017/Day01.java
+++ advent-of-code/src/main/java/aoc2017/Day01.java
@@ -1,12 +1,32 @@
 package aoc2017;
 
-public class Day01 {
+import utils.FileUtils;
+import utils.enums.Day;
+
+public class Day01 extends AoC2017 {
+
+    private final Day day = Day.DAY_01;
+    private final String input = FileUtils.getLine(year, day);
 
     public int solvePart1() {
-        return 0;
+        int sum = 0;
+        char[] captcha = input.toCharArray();
+        for (int i = 0; i < captcha.length; i++) {
+            if (captcha[i] == captcha[(i + 1) % captcha.length]) {
+                sum += Character.getNumericValue(captcha[i]);
+            }
+        }
+        return sum;
     }
 
     public int solvePart2() {
-        return 0;
+        char[] captcha = input.toCharArray();
+        int sum = 0, offset = captcha.length / 2;
+        for (int i = 0; i < captcha.length; i++) {
+            if (captcha[i] == captcha[(i + offset) % captcha.length]) {
+                sum += Character.getNumericValue(captcha[i]);
+            }
+        }
+        return sum;
     }
 }
