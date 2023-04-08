3d8a8961d934ea1716a2e5acd05703e3a98def4d
romain albisson <albisson.romain@gmail.com>
2023-04-08 16:04:46 +0200

    Day 2 refactor

--- advent-of-code/src/main/java/aoc2022/Day02.java
+++ advent-of-code/src/main/java/aoc2022/Day02.java
@@ -1,27 +1,23 @@
 package aoc2022;
 
+import static utils.FileUtils.getAllLines;
+
 import java.util.List;
 
 import aoc2022.classes.RockPaperScissors;
-import static utils.FileUtils.getAllLines;
 import utils.enums.Day;
 
 public class Day02 extends AoC2022 {
 
     private final Day day = Day.DAY_02;
-    private final List<String> input = getAllLines(year, day);
+    private final List<RockPaperScissors> input = getAllLines(year, day).stream()
+            .map(RockPaperScissors::new).toList();
 
     public int solvePart1() {
-        return input.stream()
-                .map(RockPaperScissors::new)
-                .mapToInt(RockPaperScissors::getScorePart1)
-                .sum();
+        return input.stream().mapToInt(RockPaperScissors::getScorePart1).sum();
     }
 
     public int solvePart2() {
-        return input.stream()
-                .map(RockPaperScissors::new)
-                .mapToInt(RockPaperScissors::getScorePart2)
-                .sum();
+        return input.stream().mapToInt(RockPaperScissors::getScorePart2).sum();
     }
 }
