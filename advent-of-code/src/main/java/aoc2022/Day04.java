2c24feb6f0cc2c5afe29c31f2a23153522f97dfc
romain albisson <albisson.romain@gmail.com>
2023-04-08 16:06:18 +0200

    Day 04 refactor

--- advent-of-code/src/main/java/aoc2022/Day04.java
+++ advent-of-code/src/main/java/aoc2022/Day04.java
@@ -1,27 +1,23 @@
 package aoc2022;
 
+import static utils.FileUtils.getAllLines;
+
 import java.util.List;
 
 import aoc2022.classes.AssignmentPair;
-import static utils.FileUtils.getAllLines;
 import utils.enums.Day;
 
 public class Day04 extends AoC2022 {
 
     private final Day day = Day.DAY_04;
-    private final List<String> input = getAllLines(year, day);
+    private final List<AssignmentPair> input = getAllLines(year, day).stream()
+            .map(AssignmentPair::new).toList();
 
     public int solvePart1() {
-        return (int) input.stream()
-                .map(AssignmentPair::new)
-                .filter(AssignmentPair::hasFullyContainedAssignment)
-                .count();
+        return (int) input.stream().filter(AssignmentPair::hasFullyContainedAssignment).count();
     }
 
     public int solvePart2() {
-        return (int) input.stream()
-                .map(AssignmentPair::new)
-                .filter(AssignmentPair::hasOverlappingAssignment)
-                .count();
+        return (int) input.stream().filter(AssignmentPair::hasOverlappingAssignment).count();
     }
 }
