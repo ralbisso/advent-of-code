e436e570f53eafbe89f9b5d18768142f4b7d0728
romain albisson <albisson.romain@gmail.com>
2023-04-12 14:44:33 +0200

    2015 Day 4: The Ideal Stocking Stuffer

--- advent-of-code/src/main/java/aoc2015/Day04.java
+++ advent-of-code/src/main/java/aoc2015/Day04.java
@@ -1,21 +1,25 @@
 package aoc2015;
 
-import java.util.List;
+import static utils.FileUtils.getLine;
 
-import static utils.FileUtils.getAllLines;
+import org.apache.commons.codec.digest.DigestUtils;
+
 import utils.enums.Day;
 
 public class Day04 extends AoC2015 {
 
     private final Day day = Day.DAY_04;
-    private final List<String> input = getAllLines(year, day);
+    private final String input = getLine(year, day);
 
     public int solvePart1() {
-        System.out.println(input);
-        return 0;
+        int answer = 0;
+        while (!DigestUtils.md5Hex(input + ++answer).startsWith("00000"));
+        return answer;
     }
 
     public int solvePart2() {
-        return 0;
+        int answer = 0;
+        while (!DigestUtils.md5Hex(input + ++answer).startsWith("000000"));
+        return answer;
     }
 }
