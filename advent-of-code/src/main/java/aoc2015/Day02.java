bcd3faf1b7e1225d755328a09182a2769e0ce6d1
romain albisson <albisson.romain@gmail.com>
2023-04-12 18:42:49 +0200

    Refactor

--- advent-of-code/src/main/java/aoc2015/Day02.java
+++ advent-of-code/src/main/java/aoc2015/Day02.java
@@ -2,7 +2,7 @@ package aoc2015;
 
 import static utils.FileUtils.getAllLines;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import aoc2015.classes.Present;
 import utils.enums.Day;
@@ -10,13 +10,13 @@ import utils.enums.Day;
 public class Day02 extends AoC2015 {
 
     private final Day day = Day.DAY_02;
-    private final List<Present> input = getAllLines(year, day).stream().map(Present::new).toList();
+    private final Stream<Present> input = getAllLines(year, day).stream().map(Present::new);
 
     public int solvePart1() {
-        return input.stream().mapToInt(Present::getWrappingPaperArea).sum();
+        return input.mapToInt(Present::getWrappingPaperArea).sum();
     }
 
     public int solvePart2() {
-        return input.stream().mapToInt(Present::getRibbonLength).sum();
+        return input.mapToInt(Present::getRibbonLength).sum();
     }
 }
