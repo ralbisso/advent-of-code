84ae753ee942204d3c6c4d87b3391bffd848d5e1
romain albisson <albisson.romain@gmail.com>
2023-04-12 14:21:31 +0200

    2015 Day 3: Perfectly Spherical Houses in a Vacuum

--- advent-of-code/src/main/java/aoc2015/Day03.java
+++ advent-of-code/src/main/java/aoc2015/Day03.java
@@ -1,21 +1,49 @@
 package aoc2015;
 
-import java.util.List;
+import static utils.FileUtils.getLineAsCharArray;
 
-import static utils.FileUtils.getAllLines;
+import java.util.HashSet;
+import java.util.Set;
+
+import classes.Position;
 import utils.enums.Day;
 
 public class Day03 extends AoC2015 {
 
     private final Day day = Day.DAY_03;
-    private final List<String> input = getAllLines(year, day);
+    private final char[] input = getLineAsCharArray(year, day);
 
     public int solvePart1() {
-        System.out.println(input);
-        return 0;
+        Set<Position> houses = new HashSet<>();
+        Position santasPosition = new Position(0, 0);
+        houses.add(santasPosition);
+        for (char c : input) {
+            move(santasPosition, c);
+            houses.add(santasPosition.getNewPosition());
+        }
+        return houses.size();
     }
 
     public int solvePart2() {
-        return 0;
+        Set<Position> houses = new HashSet<>();
+        Position santasPosition = new Position(0, 0);
+        Position robotPosition = new Position(0, 0);
+        houses.add(santasPosition);
+        for (int i = 0; i < input.length; i += 2) {
+            move(santasPosition, input[i]);
+            houses.add(santasPosition.getNewPosition());
+            move(robotPosition, input[i + 1]);
+            houses.add(robotPosition.getNewPosition());
+        }
+        return houses.size();
+    }
+
+    private void move(Position position, char c) {
+        switch (c) {
+        case '^' -> position.moveNorth();
+        case 'v' -> position.moveSouth();
+        case '>' -> position.moveEast();
+        case '<' -> position.moveWest();
+        }
     }
 }
