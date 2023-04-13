515b24c2e282defbe182d4aef3d93146a7bdd19a
romain albisson <albisson.romain@gmail.com>
2023-04-13 11:28:35 +0200

    2015 Day 7: Some Assembly Required

--- advent-of-code/src/main/java/aoc2015/Day07.java
+++ advent-of-code/src/main/java/aoc2015/Day07.java
@@ -1,21 +1,98 @@
 package aoc2015;
 
-import java.util.List;
-
 import static utils.FileUtils.getAllLines;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import utils.enums.Day;
 
 public class Day07 extends AoC2015 {
 
     private final Day day = Day.DAY_07;
     private final List<String> input = getAllLines(year, day);
+    private final static String AND = "AND";
+    private final static String OR = "OR";
+    private final static String LSHIFT = "LSHIFT";
+    private final static String RSHIFT = "RSHIFT";
 
     public int solvePart1() {
-        System.out.println(input);
+        Map<String, Integer> circuit = new HashMap<>();
+
+        while (circuit.size() < input.size()) {
+            for (String wire : input) {
+                String[] split = wire.split(" -> ");
+                String in = split[0], out = split[1];
+                if (!circuit.containsKey(out)) {
+                    Integer signal = getSignal(in, circuit);
+                    if (signal != null) {
+                        if ("a".equals(out)) {
+                            return signal;
+                        }
+                        circuit.put(out, signal);
+                    }
+                }
+            }
+        }
         return 0;
     }
 
     public int solvePart2() {
+        Map<String, Integer> circuit = new HashMap<>();
+        circuit.put("b", 46065);
+        while (circuit.size() < input.size()) {
+            for (String wire : input) {
+                String[] split = wire.split(" -> ");
+                String in = split[0], out = split[1];
+                if (!circuit.containsKey(out) && !"b".equals(out)) {
+                    Integer signal = getSignal(in, circuit);
+                    if (signal != null) {
+                        if ("a".equals(out)) {
+                            return signal;
+                        }
+                        circuit.put(out, signal);
+                    }
+                }
+            }
+        }
         return 0;
+    }
+
+    private Integer getSignal(String in, Map<String, Integer> circuit) {
+        Integer signal = null;
+        String[] split = in.split(" ");
+        switch (split.length) {
+        case 1 -> signal = getValue(split[0], circuit);
+        case 2 -> {
+            Integer wire = getValue(split[1], circuit);
+            if (wire != null) {
+                signal = ~wire;
+            }
+        }
+        case 3 -> {
+            Integer wire1 = getValue(split[0], circuit);
+            Integer wire2 = getValue(split[2], circuit);
+            if (wire1 != null && wire2 != null) {
+                switch (split[1]) {
+                case AND -> signal = wire1 & wire2;
+                case OR -> signal = wire1 | wire2;
+                case LSHIFT -> signal = wire1 << wire2;
+                case RSHIFT -> signal = wire1 >> wire2;
+                }
+            }
+        }
+        }
+        return signal;
+    }
+
+    private Integer getValue(String in, Map<String, Integer> circuit) {
+        Integer value;
+        try {
+            value = Integer.parseInt(in);
+        } catch (NumberFormatException nfe) {
+            value = circuit.get(in);
+        }
+        return value;
     }
 }
