0d7a20a158ddc428d36ce6c961f24999dedfa8e7
albis <albis@pc-romain>
2023-04-25 15:25:41 +0200

    2016 Day 1 - refactor

--- 2016/src/main/java/aoc/Day01.java
+++ 2016/src/main/java/aoc/Day01.java
@@ -3,57 +3,39 @@ package aoc;
 import static enums.Day.DAY_01;
 import static utils.FileUtils.getLine;
 
-import enums.Direction;
+import java.util.HashSet;
+
+import classes.Position;
 
 public class Day01 extends AoC2016 {
 
-    private final String[] input = getLine(DAY_01).split(", ");
+	private final String[] input = getLine(DAY_01).split(", ");
 
-    public int solvePart1() {
-        int facing = 0; // Direction index: NORTH
-        int[] position = { 0, 0 };
-        for (String instruction : input) {
-            facing = move(facing, position, instruction);
-        }
-        return getManhattanDistance(position);
-    }
+	public int solvePart1() {
+		var position = new Position(0, 0);
+		for (String instruction : input) {
+			position.turnAndWalk(instruction);
+		}
+		return getManhattanDistance(position);
+	}
 
-    public int solvePart2() {
-        int facing = 0; // Direction index: NORTH
-        int[] position = { 0, 0 };
-        for (String instruction : input) {
-            facing = move(facing, position, instruction);
-        }
-        return getManhattanDistance(position);
-    }
+	public int solvePart2() {
+		var position = new Position(0, 0);
+		var memo = new HashSet<>();
+		for (String instruction : input) {
+			position.turn(instruction.charAt(0));
+			int steps = Integer.parseInt(instruction.substring(1));
+			for (int i = 0; i < steps; i++) {
+				position.walk(1);
+				if (!memo.add(position.getNewPosition())) {
+					return getManhattanDistance(position);
+				}
+			}
+		}
+		return 0;
+	}
 
-    private int move(int facing, int[] position, String instruction) {
-        facing = turn(facing, instruction.charAt(0));
-        var direction = Direction.values()[facing];
-        int steps = Integer.parseInt(instruction.substring(1));
-        walk(position, direction, steps);
-        return facing;
-    }
-
-    private int turn(int facing, char instruction) {
-        if ('R' == instruction) {
-            facing = (facing + 1) % 4;
-        } else {
-            facing = ((facing - 1) + 4) % 4;
-        }
-        return facing;
-    }
-
-    private void walk(int[] position, Direction direction, int steps) {
-        switch (direction) {
-        case NORTH -> position[0] += steps;
-        case EAST -> position[1] += steps;
-        case SOUTH -> position[0] -= steps;
-        case WEST -> position[1] -= steps;
-        }
-    }
-
-    private int getManhattanDistance(int[] position) {
-        return Math.abs(position[0]) + Math.abs(position[1]);
-    }
+	private int getManhattanDistance(Position position) {
+		return Math.abs(position.getX()) + Math.abs(position.getY());
+	}
 }
--- common/src/main/java/classes/Position.java
+++ common/src/main/java/classes/Position.java
@@ -2,50 +2,110 @@ package classes;
 
 import java.util.Objects;
 
+import enums.Direction;
+
 public class Position {
 
-    private int x;
-    private int y;
+	private int x;
+	private int y;
+	private Direction facing = Direction.NORTH;
 
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    public void moveNorth() {
-        y++;
-    }
+	public int getX() {
+		return x;
+	}
 
-    public void moveSouth() {
-        y--;
-    }
+	public int getY() {
+		return y;
+	}
 
-    public void moveEast() {
-        x++;
-    }
+	public void turnAndWalk(String instruction) {
+		turn(instruction.charAt(0));
+		walk(Integer.parseInt(instruction.substring(1)));
+	}
 
-    public void moveWest() {
-        x--;
-    }
+	public void turn(char direction) {
+		if ('L' == direction) {
+			turnLeft();
+		} else {
+			turnRight();
+		}
+	}
 
-    public Position getNewPosition() {
-        return new Position(x, y);
-    }
+	private void turnLeft() {
+		facing = switch (facing) {
+		case NORTH -> Direction.EAST;
+		case EAST -> Direction.SOUTH;
+		case SOUTH -> Direction.WEST;
+		case WEST -> Direction.NORTH;
+		};
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        if (this == obj) {
-            return true;
-        }
-        Position other = (Position) obj;
-        return Objects.equals(this.x, other.x) && Objects.equals(this.y, other.y);
-    }
+	private void turnRight() {
+		facing = switch (facing) {
+		case NORTH -> Direction.WEST;
+		case EAST -> Direction.NORTH;
+		case SOUTH -> Direction.EAST;
+		case WEST -> Direction.SOUTH;
+		};
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.x, this.y);
-    }
+	public void walk(int steps) {
+		switch (facing) {
+		case EAST -> walkEast(steps);
+		case NORTH -> walkNorth(steps);
+		case SOUTH -> walkSouth(steps);
+		case WEST -> walkWest(steps);
+		}
+	}
+
+	public void walk(Direction direction) {
+		switch (direction) {
+		case EAST -> walkEast(1);
+		case NORTH -> walkNorth(1);
+		case SOUTH -> walkSouth(1);
+		case WEST -> walkWest(1);
+		}
+	}
+
+	private void walkEast(int steps) {
+		x += steps;
+	}
+
+	private void walkNorth(int steps) {
+		y += steps;
+	}
+
+	private void walkSouth(int steps) {
+		y -= steps;
+	}
+
+	private void walkWest(int steps) {
+		x -= steps;
+	}
+
+	public Position getNewPosition() {
+		return new Position(x, y);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		Position other = (Position) obj;
+		return this.x == other.x && this.y == other.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.x, this.y);
+	}
 }
