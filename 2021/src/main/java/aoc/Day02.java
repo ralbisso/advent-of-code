e6acb632f88c9afacd00e98f03c4dee35aed9499
albis <albis@pc-romain>
2023-04-25 20:53:59 +0200

    2021 Day 2: Dive!

--- 2021/src/main/java/aoc/Day02.java
+++ 2021/src/main/java/aoc/Day02.java
@@ -7,14 +7,40 @@ import java.util.List;
 
 public class Day02 extends AoC2021 {
 
-    private final List<String> input = getAllLines(DAY_02);
+	private final List<String> input = getAllLines(DAY_02);
 
-    public int solvePart1() {
-        System.out.println(input);
-        return 0;
-    }
+	private final static String FORWARD = "forward";
+	private final static String DOWN = "down";
+	private final static String UP = "up";
 
-    public int solvePart2() {
-        return 0;
-    }
+	public int solvePart1() {
+		int horizontalPosition = 0, depth = 0;
+		for (String instruction : input) {
+			String[] split = instruction.split(" ");
+			int units = Integer.parseInt(split[1]);
+			switch (split[0]) {
+			case FORWARD -> horizontalPosition += units;
+			case DOWN -> depth += units;
+			case UP -> depth -= units;
+			}
+		}
+		return horizontalPosition * depth;
+	}
+
+	public int solvePart2() {
+		int horizontalPosition = 0, depth = 0, aim = 0;
+		for (String instruction : input) {
+			String[] split = instruction.split(" ");
+			int units = Integer.parseInt(split[1]);
+			switch (split[0]) {
+			case FORWARD -> {
+				horizontalPosition += units;
+				depth += aim * units;
+			}
+			case DOWN -> aim += units;
+			case UP -> aim -= units;
+			}
+		}
+		return horizontalPosition * depth;
+	}
 }
