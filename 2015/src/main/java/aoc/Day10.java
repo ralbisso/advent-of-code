86e592efe7c55b6a2acdc41facbd85e965a4f0dd
Romain Albisson <albisson.romain@gmail.com>
2023-05-04 16:26:47 +0200

    2015 Day 10: Elves Look, Elves Say

--- 2015/src/main/java/aoc/Day10.java
+++ 2015/src/main/java/aoc/Day10.java
@@ -1,20 +1,39 @@
 package aoc;
 
 import static enums.Day.DAY_10;
-import static utils.FileUtils.getAllLines;
-
-import java.util.List;
+import static utils.FileUtils.getLine;
 
 public class Day10 extends AoC2015 {
 
-	private final List<String> input = getAllLines(DAY_10);
+	private final String input = getLine(DAY_10);
 
 	public int solvePart1() {
-		System.out.println(input);
-		return 0;
+		return lookAndSay(new StringBuilder(input), 40);
 	}
 
 	public int solvePart2() {
-		return 0;
+		return lookAndSay(new StringBuilder(input), 50);
+	}
+
+	private int lookAndSay(StringBuilder sequence, int times) {
+		for (int i = 0; i < times; i++) {
+			sequence = lookAndSay(sequence);
+		}
+		return sequence.length();
+	}
+
+	private StringBuilder lookAndSay(StringBuilder sequence) {
+		StringBuilder next = new StringBuilder();
+		for (int i = 0; i < sequence.length(); i++) {
+			char c = sequence.charAt(i);
+			int count = 1;
+			while (i + 1 < sequence.length() && c == sequence.charAt(i + 1)) {
+				count++;
+				i++;
+			}
+			next.append(count);
+			next.append(c);
+		}
+		return next;
 	}
 }
