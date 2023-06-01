e38bba80cb4d53eb9e3bb4e2c7a306c75294dc6c
Romain Albisson <albisson.romain@gmail.com>
2023-06-01 13:42:34 +0200

    2017 - Day 11: Hex Ed

--- 2017/src/main/java/aoc/Day11.java
+++ 2017/src/main/java/aoc/Day11.java
@@ -1,20 +1,66 @@
 package aoc;
 
+import static classes.Split.COMMA;
 import static enums.Day.DAY_11;
-import static utils.FileUtils.getAllLines;
-
-import java.util.List;
+import static utils.FileUtils.getLine;
 
 public class Day11 extends AoC2017 {
 
-    private final List<String> input = getAllLines(DAY_11);
+	private final String[] input = getLine(DAY_11).split(COMMA);
 
-    public int solvePart1() {
-        System.out.println(input);
-        return 0;
-    }
+	private final static String NORTH = "n";
+	private final static String NORTH_EAST = "ne";
+	private final static String NORTH_WEST = "nw";
+	private final static String SOUTH = "s";
+	private final static String SOUTH_EAST = "se";
+	private final static String SOUTH_WEST = "sw";
 
-    public int solvePart2() {
-        return 0;
-    }
+	public int solvePart1() {
+		int[] position = { 0, 0 };
+		for (String direction : input) {
+			move(position, direction);
+		}
+		return getFewestNumberOfSteps(position);
+	}
+
+	public int solvePart2() {
+		int[] position = { 0, 0 };
+		int max = 0;
+		for (String direction : input) {
+			move(position, direction);
+			int fewestNumberOfSteps = getFewestNumberOfSteps(position);
+			if (fewestNumberOfSteps > max) {
+				max = fewestNumberOfSteps;
+			}
+		}
+		return max;
+	}
+
+	private void move(int[] position, String mv) {
+		switch (mv) {
+		case NORTH -> position[1] += 2;
+		case SOUTH -> position[1] -= 2;
+		case NORTH_EAST -> {
+			position[0]++;
+			position[1]++;
+		}
+		case NORTH_WEST -> {
+			position[0]--;
+			position[1]++;
+		}
+		case SOUTH_EAST -> {
+			position[0]++;
+			position[1]--;
+		}
+		case SOUTH_WEST -> {
+			position[0]--;
+			position[1]--;
+		}
+		}
+	}
+
+	private int getFewestNumberOfSteps(int[] position) {
+		int x = Math.abs(position[0]), y = Math.abs(position[1]);
+		return (x + y + 1) / 2;
+	}
 }
